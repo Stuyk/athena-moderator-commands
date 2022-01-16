b1f09df51b2d32d184e51be556ce4f0bf57e0153
Der Lord! <82890183+Booster1212@users.noreply.github.com>
2022-01-16 19:47:52 +0100

    Changing Readme (#2)
    * Rewrite to uid
    
    * Change Readme and some cmds.
    
    * Optionalize dimension

--- src/commands/unban.ts
+++ src/commands/unban.ts
@@ -3,11 +3,11 @@ import { playerFuncs } from '../../../../server/extensions/extPlayer';
 import { AdminController } from '../../../../server/systems/admin';
 
 export async function handleUnbanCmd(player: alt.Player, discordIdentifier: string) {
-    if(!player || !player.valid) {
+    if (!player || !player.valid) {
         return;
     }
     const unbanned = AdminController.unbanPlayer(discordIdentifier);
-    if(unbanned) {
+    if (unbanned) {
         playerFuncs.emit.message(player, `Unbanned ${discordIdentifier} (Discord)`);
         alt.logWarning(`${discordIdentifier} (Discord) was unbanned by ${player.data.name}`);
         return;
